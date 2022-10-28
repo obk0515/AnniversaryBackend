866b3b3b0e854f921aa7ef93f95a5bdf4adb9cee
obk <543368874@qq.com>
2022-10-28 23:49:40 +0800

    初始项目

---
+++ src/test/java/com/fzu/BaseApplicationTests.java
@@ -0,0 +1,11 @@
+package com.fzu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApplicationTests {
+    @Test
+    void contextLoads(){
+    }
+}
